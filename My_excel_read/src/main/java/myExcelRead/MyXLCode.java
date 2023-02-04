233d3d076a2b8462adc5b78b7161368ef481a2eb
PC <PC@192.168.0.107>
2023-02-04 22:54:56 +0530

    This is excel commit

---
+++ My_excel_read/src/main/java/myExcelRead/MyXLCode.java
@@ -0,0 +1,37 @@
+package myExcelRead;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class MyXLCode {
+	static FileInputStream f;
+	static XSSFWorkbook wb;
+	static XSSFSheet sh;
+
+	public static String stringDataRead(int row, int col) throws IOException { //to print String value
+		//f = new FileInputStream("C:\\Users\\dhany\\OneDrive\\Desktop\\Excel01.xlsx");
+		f = new FileInputStream("C:\\Users\\PC\\Desktop\\Excelread.xlsx");
+		wb = new XSSFWorkbook(f);
+		sh = wb.getSheet("Excel1");
+		Row r = sh.getRow(row);
+		Cell c = r.getCell(col);
+		return c.getStringCellValue();
+	}
+
+	public static int integerDataRead(int row, int col) throws IOException
+	{
+		f = new FileInputStream("C:\\Users\\PC\\Desktop\\Excelread.xlsx");
+		wb = new XSSFWorkbook(f);
+		sh = wb.getSheet("Excel1");
+		Row r = sh.getRow(row);
+		Cell c = r.getCell(col);
+		return (int) c.getNumericCellValue();
+		
+	}
+
+}
